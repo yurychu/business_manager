19edad34ee34e1d06b7bba0ce69ef50cbe9f2013
yurychu <tesewar@mail.ru>
2016-04-15 01:01:43 +0300

    что то работает

--- business_manager/purposes/views.py
+++ business_manager/purposes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -14,13 +14,20 @@ class PurposesListView(ListView):
 class PurposeCreateView(SuccessMessageMixin, CreateView):
     model = Purpose
     form_class = PurposeForm
-    # success_url = '/purposes/'
+    success_url = '/purposes/'
     success_message = 'Новая цель успешно создана!'
 
     def get_form_kwargs(self):
         kwargs = super(PurposeCreateView, self).get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
+
+    def form_valid(self, form):
+        purpose = form.save(commit=False)
+        purpose.user = self.request.user
+        purpose.save()
+        super(PurposeCreateView, self).form_valid(form)
+        return HttpResponseRedirect(self.success_url)
 
 
 class PurposeUpdateView(SuccessMessageMixin, UpdateView):
