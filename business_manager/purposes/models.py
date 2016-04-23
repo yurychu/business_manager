3e1edf10360af7270b3632c620b3bfb1976ef79a
yurychu <tesewar@mail.ru>
2016-04-23 19:03:15 +0300

    добавление Штампа Времени

--- business_manager/purposes/models.py
+++ business_manager/purposes/models.py
@@ -2,8 +2,10 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.conf import settings
 
+from core.models import TimeStampedModel
 
-class Purpose(models.Model):
+
+class Purpose(TimeStampedModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
