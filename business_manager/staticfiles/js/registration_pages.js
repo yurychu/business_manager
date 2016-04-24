c375760078397799b43ebab3ed95a445481b58de
yurychu <tesewar@mail.ru>
2016-04-24 21:38:15 +0300

    оформление логина

--- business_manager/staticfiles/js/registration_pages.js
+++ business_manager/staticfiles/js/registration_pages.js
@@ -1,3 +1,4 @@
 $('label').addClass('sr-only')
 $('input').addClass('form-control')
-$('')
+$('#id_username').attr({'placeholder': 'Логин','required': 'required'}).focus()
+$('#id_password').attr({'placeholder': 'Пароль', 'required': 'required'})
